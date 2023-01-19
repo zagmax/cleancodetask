5bfc503456eaa437e807ba2ceca79c3d0e390f37
Maksym Zahreba <maksym_zahreba@epam.com>
2023-01-19 16:13:31 +0200

    import "*" change

--- Java/src/main/java/Airport.java
+++ Java/src/main/java/Airport.java
@@ -4,7 +4,8 @@ import Planes.Plane;
 import Planes.experimentalPlane;
 import models.MilitaryType;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 
